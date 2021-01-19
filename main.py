951fd514af6e7a63586cfa6f9c9d7f45b918fc66
Tharushka05 <77673732+Tharushka05@users.noreply.github.com>
2021-01-19 19:16:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter a number;"))
+operation = input("Enter an operation;")
+num2 = float(input("Enter another number;"))
+
+if operation == "+":
+    print(num1+num2)
+if operation == "-":
+    print(num1-num2)
+if operation == "*":
+    print(num1*num2)
+if operation == "/":
+    print(num1/num2)
+
+
